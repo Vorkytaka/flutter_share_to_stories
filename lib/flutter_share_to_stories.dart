cad06e567e4fe7b9717d9e34b1d1b0ab0e730b45
Vorkytaka <vorkytaka@gmail.com>
2020-12-15 01:34:10 +0300

    • Add base for `ShareToStories`

--- lib/flutter_share_to_stories.dart
+++ lib/flutter_share_to_stories.dart
@@ -1,1 +1,17 @@
-class ShareToStories {}
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+
+class ShareToStories {
+  static const String CHANNEL_NAME = "vrk.tk/share_to_stories";
+  static const MethodChannel CHANNEL = MethodChannel(CHANNEL_NAME);
+
+  static Future<void> shareToInstagram({
+    Uri backgroundAssetUri,
+    Uri stickerAssetUri,
+    Color topColor,
+    Color bottomColor,
+  }) async {
+    assert(backgroundAssetUri != null || stickerAssetUri != null);
+  }
+}
