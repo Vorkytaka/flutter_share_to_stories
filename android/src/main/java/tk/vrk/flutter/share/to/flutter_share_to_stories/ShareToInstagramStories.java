7ccd33d2a8d420370ce5112d2ae79c45a041f556
Vorkytaka <vorkytaka@gmail.com>
2020-12-18 02:25:09 +0300

    • Handle background uri • Add `FileProvider` for plugin • Use `FileProvider` for push image to the instagram stories

--- android/src/main/java/tk/vrk/flutter/share/to/flutter_share_to_stories/ShareToInstagramStories.java
+++ android/src/main/java/tk/vrk/flutter/share/to/flutter_share_to_stories/ShareToInstagramStories.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 public class ShareToInstagramStories {
     private Context context;
@@ -24,10 +30,17 @@ public class ShareToInstagramStories {
 
         Intent intent = new Intent("com.instagram.share.ADD_TO_STORY");
 
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra("source_application", context.getPackageName());
+
         if (backgroundAssetUri != null) {
-            // TODO: get mime type
-            intent.setDataAndType(backgroundAssetUri, "image/jpeg");
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            File file = new File(backgroundAssetUri.getPath());
+            Uri uri = FileProvider.getUriForFile(context, context.getPackageName() + ".flutter.share_to_stories", file);
+
+            String ext = MimeTypeMap.getFileExtensionFromUrl(uri.getPath());
+            String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext);
+
+            intent.setDataAndType(uri, mimeType != null ? mimeType : "image/jpeg");
         }
 
         if (stickerAssetUri != null) {
@@ -42,6 +55,8 @@ public class ShareToInstagramStories {
             intent.putExtra("bottom_background_color", bottomColor);
         }
 
-        // TODO: Start activity
+        if (activity.getPackageManager().resolveActivity(intent, 0) != null) {
+            activity.startActivityForResult(intent, 0);
+        }
     }
 }
