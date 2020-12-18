04f41fbcb015b488947a8aa01278addeb7e5647f
Vorkytaka <vorkytaka@gmail.com>
2020-12-18 03:19:09 +0300

    • Create base for example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +11,105 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Welcome!"),
+        appBar: AppBar(
+          title: Text("Share to Instagram stories"),
         ),
+        body: SelectorWidget(),
       ),
+    );
+  }
+}
+
+class SelectorWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SelectorWidgetState();
+}
+
+class _SelectorWidgetState extends State<SelectorWidget> {
+  File _background;
+  File _sticker;
+  Color _topColor;
+  Color _bottomColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: ScrollPhysics(),
+      children: [
+        GridView.count(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Placeholder(
+                  strokeWidth: 0.5,
+                  color: Colors.grey,
+                ),
+                ElevatedButton(
+                  child: Text("Select Background Asset"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Placeholder(
+                  strokeWidth: 0.5,
+                  color: Colors.grey,
+                ),
+                ElevatedButton(
+                  child: Text("Select Sticker Asset"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Placeholder(
+                  strokeWidth: 0.5,
+                  color: Colors.grey,
+                ),
+                ElevatedButton(
+                  child: Text("Select Top Color"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Placeholder(
+                  strokeWidth: 0.5,
+                  color: Colors.grey,
+                ),
+                ElevatedButton(
+                  child: Text("Select Bottom Color"),
+                  onPressed: () {},
+                ),
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              child: Text("SHARE TO INSTAGRAM"),
+              onPressed: null,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
