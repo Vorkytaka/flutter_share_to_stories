567187cbcd138c6f450dbd677fd848bf6673f588
Vorkytaka <vorkytaka@gmail.com>
2020-12-20 04:59:26 +0300

    • Test

--- test/share_to_stories_test.dart
+++ test/share_to_stories_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_share_to_stories/share_to_stories.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart' show TestWidgetsFlutterBinding;
 import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   MockMethodChannel mockMethodChannel;
 
   setUp(() {
@@ -11,6 +14,13 @@ void main() {
     ShareToStories.CHANNEL.setMockMethodCallHandler((call) async {
       await mockMethodChannel.invokeMethod<void>(call.method, call.arguments);
     });
+  });
+
+  test("Share with nulls will throw an error", () {
+    expect(
+      () => ShareToStories.shareToInstagram(),
+      throwsA(const TypeMatcher<AssertionError>()),
+    );
   });
 }
 
